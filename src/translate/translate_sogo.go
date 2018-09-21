9fd1d8a50e35a0ea15e39e577edd4b1081d66f5d
hiscaler <hiscaler@gmail.com>
2018-09-21 19:54:05 +0800

    使用 goroutine 进行节点数据翻译

--- src/translate/translate_sogo.go
+++ src/translate/translate_sogo.go
@@ -11,7 +11,6 @@ import (
 	"errors"
 	"encoding/hex"
 	"net/url"
-	"sync"
 	"runtime"
 )
 
@@ -41,104 +40,100 @@ var errorCodes = map[string]string{
 	"1050":  "内部服务错误",
 }
 
-var wg sync.WaitGroup
-
 func (t *SogoTranslate) Req(i int, s string, in chan<- string) (string, error) {
-	letters := []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
-	randSeq := func(n int) string {
-		b := make([]rune, n)
-		for i := range b {
-			b[i] = letters[rand.Intn(len(letters))]
+	if len(s) > 0 {
+		letters := []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+		randSeq := func(n int) string {
+			b := make([]rune, n)
+			for i := range b {
+				b[i] = letters[rand.Intn(len(letters))]
+			}
+			return string(b)
 		}
-		return string(b)
-	}
-	client := &http.Client{}
-	salt := randSeq(12)
-	account := t.GetRandomAccount()
-	mdx := md5.New()
-	mdx.Write([]byte(account.PID + s + salt + account.SecretKey))
-	sign := hex.EncodeToString(mdx.Sum(nil))
-	fields := map[string]string{
-		"q":    s,
-		"from": t.From,
-		"to":   t.To,
-		"pid":  account.PID,
-		"salt": salt,
-		"sign": sign,
-	}
-	body := url.Values{}
-	for k, v := range fields {
-		body.Set(k, v)
-	}
-	req, err := http.NewRequest("POST", "http://fanyi.sogou.com/reventondc/api/sogouTranslate", strings.NewReader(body.Encode()))
-	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
-	req.Header.Set("Accept", "application/json")
-	if err == nil {
-		resp, err := client.Do(req)
-		if err == nil && resp.StatusCode == 200 {
-			body, err := ioutil.ReadAll(resp.Body)
-			if err == nil {
-				sogoResponse := &SogoResponse{}
-				err = json.Unmarshal([]byte(body), &sogoResponse)
+		client := &http.Client{}
+		salt := randSeq(12)
+		account := t.GetRandomAccount()
+		mdx := md5.New()
+		mdx.Write([]byte(account.PID + s + salt + account.SecretKey))
+		sign := hex.EncodeToString(mdx.Sum(nil))
+		fields := map[string]string{
+			"q":    s,
+			"from": t.From,
+			"to":   t.To,
+			"pid":  account.PID,
+			"salt": salt,
+			"sign": sign,
+		}
+		body := url.Values{}
+		for k, v := range fields {
+			body.Set(k, v)
+		}
+		req, err := http.NewRequest("POST", "http://fanyi.sogou.com/reventondc/api/sogouTranslate", strings.NewReader(body.Encode()))
+		req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+		req.Header.Set("Accept", "application/json")
+		if err == nil {
+			resp, err := client.Do(req)
+			if err == nil && resp.StatusCode == 200 {
+				body, err := ioutil.ReadAll(resp.Body)
 				if err == nil {
-					if sogoResponse.ErrorCode == "0" {
-						if t.Config.Debug {
-							t.Logger.InfoLogger.Println(fmt.Sprintf("#%v After: %#v", i+1, sogoResponse.Translation))
-						}
-						in <- sogoResponse.Translation
-						return sogoResponse.Translation, nil
-					} else {
-						if sogoResponse.ErrorCode == "1003" || sogoResponse.ErrorCode == "1004" || sogoResponse.ErrorCode == "1005" {
-							_, err = t.updateAccount(account.PID, false)
-							if err != nil {
-								t.Logger.ErrorLogger.Println(err)
+					sogoResponse := &SogoResponse{}
+					err = json.Unmarshal([]byte(body), &sogoResponse)
+					if err == nil {
+						if sogoResponse.ErrorCode == "0" {
+							if t.Config.Debug {
+								t.Logger.InfoLogger.Println(fmt.Sprintf("#%v After: %#v", i+1, sogoResponse.Translation))
+							}
+							in <- sogoResponse.Translation
+							return sogoResponse.Translation, nil
+						} else {
+							if sogoResponse.ErrorCode == "1003" || sogoResponse.ErrorCode == "1004" || sogoResponse.ErrorCode == "1005" {
+								_, err = t.updateAccount(account.PID, false)
+								if err != nil {
+									t.Logger.ErrorLogger.Println(err)
+								}
+							}
+							msg, exists := errorCodes[sogoResponse.ErrorCode]
+							if !exists {
+								msg = sogoResponse.ErrorCode
+							}
+							if t.Config.Debug {
+								msg = fmt.Sprintf("%v (%v)", msg, s)
+							}
+
+							err = errors.New(msg)
+							if t.Config.Debug {
+								t.Logger.ErrorLogger.Println(fmt.Sprintf("#%v After: %#v", i+1, err.Error()))
 							}
 						}
-						msg, exists := errorCodes[sogoResponse.ErrorCode]
-						if !exists {
-							msg = sogoResponse.ErrorCode
-						}
+					} else {
 						if t.Config.Debug {
-							msg = fmt.Sprintf("%v (%v)", msg, s)
+							t.Logger.ErrorLogger.Println(fmt.Sprintf("#%v After: %#v", i+1, string(body)))
 						}
-
-						err = errors.New(msg)
-						if t.Config.Debug {
-							t.Logger.ErrorLogger.Println(fmt.Sprintf("#%v After: %#v", i+1, err.Error()))
-						}
-					}
-				} else {
-					if t.Config.Debug {
-						t.Logger.ErrorLogger.Println(fmt.Sprintf("#%v After: %#v", i+1, string(body)))
 					}
 				}
+				resp.Body.Close()
 			}
-			resp.Body.Close()
 		}
+		in <- s
+		return s, err
 	}
-	in <- s
 
-	return s, err
+	return s, errors.New("Text is empty.")
+
 }
 
 func (t *SogoTranslate) Do() (*SogoTranslate, error) {
 	runtime.GOMAXPROCS(runtime.NumCPU())
 	out := make(chan string, len(t.Nodes))
-	fmt.Println(len(t.Nodes))
 	for i, node := range t.Nodes {
-		fmt.Println(i)
 		t.Anatomy(node)
 		s := strings.Trim(t.currentNodeText, " \r\n\t")
 		if t.Config.Debug {
 			t.Logger.InfoLogger.Println(fmt.Sprintf("#%v Before: %#v", i+1, s))
 		}
-		if len(s) > 0 {
-			go t.Req(i, s, out)
-		}
-	}
 
-	for v := range out {
-		fmt.Println(v)
+		go t.Req(i, s, out)
+		t.currentNode.Data = <-out
 	}
 	close(out)
 
